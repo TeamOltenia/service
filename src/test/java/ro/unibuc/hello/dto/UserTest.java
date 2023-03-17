ac83592096a5830f71be1cd952bf19e23625ef04
Lolot Stefan-Bogdan <lolotstefanbogdan@gmail.com>
2023-03-17 10:50:00 +0200

    UserDTO test

---
+++ src/test/java/ro/unibuc/hello/dto/UserTest.java
@@ -0,0 +1,39 @@
+package ro.unibuc.hello.dto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class UserTest {
+
+    private User user = User.builder()
+            .id("1")
+            .firstName("firstName")
+            .lastName("lastName")
+            .email("userTest@email.com")
+            .build();
+
+    @Test
+    void testIdCorrect() {
+        assertEquals("1", user.getId());
+    }
+
+    @Test
+    void testContent() {
+        Assertions.assertAll(() -> assertEquals("firstName", user.getFirstName()),
+                () -> assertEquals("lastName", user.getLastName()),
+                () -> assertEquals("userTest@email.com", user.getEmail())
+        );
+    }
+
+    @Test
+    void testContentWrong() {
+        Assertions.assertAll(() -> assertNotEquals("firstNameWrong", user.getFirstName()),
+                () -> assertNotEquals("lastNameWrong", user.getLastName()),
+                () -> assertNotEquals("userTestWrong@email.com", user.getEmail())
+        );
+    }
+
+}
